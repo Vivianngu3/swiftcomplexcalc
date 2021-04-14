528f6d3ca3901a9edb8a6ba6eed945ddc07069df
“vivianngu3” <vn23@uw.edu>
2021-04-13 20:54:38 -0700

    section 2/3 done

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -3,6 +3,8 @@ print("Welcome back to the UW Calculator")
 // Your job is to fill out Calculator so all the expressions
 // below both compile and return "true"
 class Calculator {
+    
+   
     func add(lhs: Int, rhs: Int) -> Int {
         return lhs + rhs
     }
@@ -19,9 +21,48 @@ class Calculator {
         return lhs / rhs
     }
     
+    
     func mathOp(lhs: Int, rhs: Int, op: (_ lhs: Int, _ rhs: Int) -> Int) -> Int {
         return op(lhs, rhs)
     }
+    
+    func add(_ args: [Int]) -> Int {
+        var sum = 0
+        for i in args {
+            sum += i
+        }
+        return sum
+    }
+    
+    func multiply(_ args: [Int]) -> Int {
+        var multi = 1
+        for i in args {
+            multi *= i
+        }
+        return multi
+    }
+    
+    func count(_ args: [Int]) -> Int {
+        return args.count
+    }
+    
+    func avg(_ args: [Int]) -> Int {
+        var mean = 0
+        for i in args {
+            mean += i
+        }
+        return mean / args.count
+    }
+    
+    func mathOp(args: [Int], beg: Int, op: (_ lhs: Int, _ rhs: Int) -> Int) -> Int {
+        var outcome = beg
+        for i in args {
+            outcome = op(outcome, i)
+        }
+        return outcome
+    }
+    
+    
 }
 
 let calc = Calculator()  // Don't change this declaration name; it's used in all the tests below
@@ -40,7 +81,7 @@ calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (
 calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
     // This is the second, more terse, style; either works
 
-/* calc.add([1, 2, 3, 4, 5]) == 15
+calc.add([1, 2, 3, 4, 5]) == 15
 calc.multiply([1, 2, 3, 4, 5]) == 120
 calc.count([1, 2, 3, 4, 5, 6, 7, 8]) == 8
 calc.count([]) == 0
@@ -54,7 +95,7 @@ calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15
     // this is (((((0 op 1) op 2) op 3) op 4) op 5)
 calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1
     // this is (((((1 op 1) op 1) op 1) op 1) op 1)
-
+/*
 let p1 = (5, 5)
 let p2 = (12, -27)
 let p3 = (-4, 4)
